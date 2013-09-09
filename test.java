1aeca70502015d3c5ba57fa7e5917cb9a689954a
hoaint55 <hoaingo92@gmail.com>
2013-09-09 17:28:20 +0700

    lam test don gian

--- test.java
+++ test.java
@@ -1,7 +1,41 @@
-class  
+/* kiem thu cho ham bac nhat */
+public class test
 {
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
+	static int giaiPTB1 (int a, int b){
+		return 0;
 	}
+	static void test1
+	{
+	int ketquaThat = giaiPTB1(1, 1);
+	
+	if (ketquaThat == -1)
+		{
+			System.out.println ("success";
+		}
+		else{
+			System.out.println("failed");
+		}
+	
+	}
+	static void test2
+	{
+	int ketquaThat = giaiPTB1(10, 10);
+	
+	if (ketquaThat == 1)
+		{
+			System.out.println ("test2 success";
+		}
+		else{
+			System.out.println("test2 failed");
+		}
+	
+	}
+
 }
+	public static void main (String[] args) {
+		System.out.println("hello world");
+		
+		//test dau tien
+		test a[1]
+	}
+{
